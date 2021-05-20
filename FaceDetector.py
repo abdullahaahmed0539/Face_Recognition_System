87100face32f8e969533ca14adc354799ebba082
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2021-05-20 11:00:24 +0500

    detects face off webcam

--- FaceDetector.py
+++ FaceDetector.py
@@ -4,24 +4,26 @@ from random import randrange
 # loads some pre-trained data on face frontal from opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# choose an image to detect faces in
-img = cv2.imread('assets/cp.jpg')
+# loads webcam video
+webcam = cv2.VideoCapture(0)
+while True:
+
+    # read returns frame
+    successful_frame_read, frame = webcam.read()
+
+    # grey scale the frame
+    grey_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detects coordinates
+    face_coordinates = trained_face_data.detectMultiScale(grey_scale)
+
+    # de-structure coordinates and print rectangle
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(128,256), randrange(128,256), randrange(128,256)), 4)
+
+    # show image
+    cv2.imshow('Face Detector', frame)
+    cv2.waitKey(1)
 
 
-# grey scale the img
-grey_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# detect faces
-face_coordinates = trained_face_data.detectMultiScale(grey_scale)
-
-
-for (x, y, w, h) in face_coordinates:
-    # draw rectangle on the img
-    cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(128,256), randrange(128,256), randrange(128,256)), 4)
-
-print(face_coordinates)
-
-# shows the img
-cv2.imshow('Face Detector', img)
-cv2.waitKey()
 
