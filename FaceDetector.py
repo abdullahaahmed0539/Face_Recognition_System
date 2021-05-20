02dd2c0a2e66168efa1eb94cd764fa2b4804c8ce
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2021-05-20 10:38:46 +0500

    identifies faces off the image

--- FaceDetector.py
+++ FaceDetector.py
@@ -1,10 +1,25 @@
 import cv2
+from random import randrange
 
-# Loads some pre-trained data on face frontal from opencv
+# loads some pre-trained data on face frontal from opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # choose an image to detect faces in
-img = cv2.imread('assets/RDJ.jpg')
+img = cv2.imread('assets/cp.jpg')
+
+
+# grey scale the img
+grey_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# detect faces
+face_coordinates = trained_face_data.detectMultiScale(grey_scale)
+
+
+for (x, y, w, h) in face_coordinates:
+    # draw rectangle on the img
+    cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(128,256), randrange(128,256), randrange(128,256)), 4)
+
+print(face_coordinates)
 
 # shows the img
 cv2.imshow('Face Detector', img)
